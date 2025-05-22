41cb4eb2d3168f48f623d1313f76a1ed6131da66
unknown <prabu502@gmail.com>
2025-05-22 16:48:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello, World!</h1>');
+});
+
+app.get("/work",function(req,res)
+
+
+{
+  res.send("<h1 style='color:red'>I am vijay</h1>")
+
+});
+
+
+
+
+module.exports = app; // Export for testingnod
+
+
+app.listen(3000);
---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('responds with Hello, World!', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Hello, World!', done);
+  });
+});
